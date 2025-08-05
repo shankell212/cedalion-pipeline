7d5af71241a08facf0ff808b35f7e5a1613dfcf0
lauracarlton <lcarlton@bu.edu>
2025-08-05 14:12:58 -0400

    incorporating the correct GLM

--- workflow/scripts/modules/module_preprocess.py
+++ workflow/scripts/modules/module_preprocess.py
@@ -20,6 +20,8 @@ import cedalion.plots as plots
 from cedalion import units
 import numpy as np
 import pandas as pd
+from functools import reduce
+import operator
 
 import json
 
@@ -167,7 +169,7 @@ def pruneChannels( rec, cfg_prune ):
     return rec, chs_pruned
 
 
-def GLM(rec, rec_str, cfg_GLM, cfg_hrf, pruned_chans):
+def GLM_filter(rec, rec_str, cfg_GLM, cfg_hrf, pruned_chans):
     
     # !!! change to have this do a weighted avg of all the short chans
     # get pruned data for SSR
@@ -216,6 +218,169 @@ def GLM(rec, rec_str, cfg_GLM, cfg_hrf, pruned_chans):
              
     return rec
 
+def GLM(runs, rec_str, cfg_GLM, geo3d, pruned_chans_list):
+
+    # 1. need to concatenate runs 
+    Y_all, stim_df, runs_updated = concatenate_runs(runs, rec_str)
+
+    # 2. define design matrix
+
+    dms = glm.design_matrix.hrf_regressors(
+                                    Y_all,
+                                    stim_df,
+                                    glm.GaussianKernels(cfg_GLM['t_pre'], cfg_GLM['t_post'], cfg_GLM['t_delta'], cfg_GLM['t_std'])
+                                )
+
+    # Combine drift and short-separation regressors (if any)
+    if cfg_GLM['do_drift']:
+        drift_regressors = get_drift_regressors(runs_updated, cfg_GLM)
+        dms &= reduce(operator.and_, drift_regressors)
+
+    if cfg_GLM['do_short_sep']:
+        ss_regressors = get_short_regressors(runs_updated, pruned_chans_list, geo3d, cfg_GLM)
+        dms &= reduce(operator.and_, ss_regressors)
+
+    dms.common = dms.common.fillna(0)
+
+    # 3. get betas and covariance
+    results = glm.fit(Y_all, dms, noise_model=cfg_GLM['noise_model']) 
+    betas = results.sm.params
+    cov_params = results.sm.cov_params()
+
+    # 4. estimate HRF and MSE
+    basis_hrf = glm.GaussianKernels(cfg_GLM['t_pre'], cfg_GLM['t_post'], cfg_GLM['t_delta'], cfg_GLM['t_std'])(Y_all)
+
+    trial_type_list = stim_df['trial_type'].unique()
+
+    hrf_mse_list = []
+    hrf_estimate_list = []
+
+    for trial_type in trial_type_list:
+        
+        betas_hrf = betas.sel(regressor=betas.regressor.str.startswith(f"HRF {trial_type}"))
+        hrf_estimate = estimate_HRF_from_beta(betas_hrf, basis_hrf)
+        
+        cov_hrf = cov_params.sel(regressor_r=cov_params.regressor_r.str.startswith(f"HRF {trial_type}"),
+                            regressor_c=cov_params.regressor_c.str.startswith(f"HRF {trial_type}") 
+                                    )
+        hrf_mse = estimate_HRF_cov(cov_hrf, basis_hrf)
+
+        hrf_estimate = hrf_estimate.expand_dims({'trial_type': [ trial_type ] })
+        hrf_mse = hrf_mse.expand_dims({'trial_type': [ trial_type ] })
+
+        hrf_estimate_list.append(hrf_estimate)
+        hrf_mse_list.append(hrf_mse)
+
+    hrf_estimate = xr.concat(hrf_estimate_list, dim='trial_type')
+    hrf_estimate = hrf_estimate.pint.quantify('molar')
+
+    hrf_mse = xr.concat(hrf_mse_list, dim='trial_type')
+    hrf_mse = hrf_mse.pint.quantify('molar**2')
+
+    # set universal time so that all hrfs have the same time base 
+    fs = frequency.sampling_rate(runs[0][rec_str]).to('Hz')
+    before_samples = int(np.ceil((cfg_GLM['t_pre'] * fs).magnitude))
+    after_samples = int(np.ceil((cfg_GLM['t_post'] * fs).magnitude))
+
+    dT = np.round(1 / fs, 3)  # millisecond precision
+    n_timepoints = len(hrf_estimate.time)
+    reltime = np.linspace(-before_samples * dT, after_samples * dT, n_timepoints)
+
+    hrf_mse = hrf_mse.assign_coords({'time': reltime})
+    hrf_mse.time.attrs['units'] = 'second'
+
+    hrf_estimate = hrf_estimate.assign_coords({'time': reltime})
+    hrf_estimate.time.attrs['units'] = 'second'
+
+    return results, hrf_estimate, hrf_mse
+
+
+def estimate_HRF_cov(cov, basis_hrf):
+
+    basis_hrf = basis_hrf.rename({'component':'regressor_c'})
+    basis_hrf = basis_hrf.assign_coords(regressor_c=cov.regressor_c.values)
+
+    tmp = xr.dot(cov, basis_hrf, dims='regressor_c')
+
+    tmp = tmp.rename({'regressor_r':'regressor'})
+    basis_hrf = basis_hrf.rename({'regressor_c':'regressor'})
+
+    mse_t = xr.dot(basis_hrf, tmp, dims='regressor')
+
+    return mse_t
+
+def estimate_HRF_from_beta(betas, basis_hrf):
+        
+    basis_hrf = basis_hrf.rename({'component':'regressor'})
+    basis_hrf = basis_hrf.assign_coords(regressor=betas.regressor.values)
+
+    hrf_estimate = xr.dot(betas, basis_hrf, dims='regressor')
+
+    hrf_estimates_blcorr = hrf_estimate - hrf_estimate.sel(time = hrf_estimate.time[hrf_estimate.time<0]).mean('time')
+
+    return hrf_estimates_blcorr
+
+def get_drift_regressors(runs, cfg_GLM):
+    
+    drift_regressors = []
+    i=0
+    for i, run  in enumerate(runs):
+
+        drift = glm.design_matrix.drift_regressors(run, cfg_GLM['drift_order'])
+        drift.common = drift.common.assign_coords({'regressor': [f'Drift {x} run {i}' for x in range(cfg_GLM['drift_order']+1)]})
+        drift_regressors.append(drift)
+        
+    return drift_regressors
+
+
+def get_short_regressors(runs, pruned_chans_list, geo3d, cfg_GLM):
+    ss_regressors = []
+    i=0
+    for run, pruned_chans in zip(runs, pruned_chans_list):
+
+        rec_pruned = prune_mask_ts(run, pruned_chans) # !!! how is this affected when using pruned data
+        _, ts_short = cedalion.nirs.split_long_short_channels(
+                                rec_pruned, geo3d, distance_threshold= cfg_GLM['distance_threshold']  # !!! change to rec_pruned once NaN prob fixed
+                                )
+
+        short = glm.design_matrix.average_short_channel_regressor(ts_short)
+        short.common = short.common.reset_coords('samples', drop=True)
+        short.common = short.common.assign_coords({'regressor': [f'short run {i}']})
+        ss_regressors.append(short)
+        i = i+1
+
+    return ss_regressors
+
+def concatenate_runs(runs, rec_str):
+
+    CURRENT_OFFSET = 0
+    runs_updated = []
+    stim_updated = []
+
+    for rec in runs:
+
+        ts = rec[rec_str]
+        time = ts.time.values
+        new_time = time + CURRENT_OFFSET
+
+        ts_new = ts.copy(deep=True)
+        ts_new = ts_new.pint.to('molar')
+        ts_new = ts_new.assign_coords(time=new_time)
+
+        stim = rec.stim
+        stim_shift = stim.copy()
+        stim_shift['onset'] += CURRENT_OFFSET
+
+        stim_updated.append(stim_shift)
+        runs_updated.append(ts_new)
+
+        CURRENT_OFFSET = new_time[-1] + (time[1] - time[0])
+
+    Y_all = xr.concat(runs_updated, dim='time')
+    Y_all.time.attrs['units'] = units.s
+    stim_df = pd.concat(stim_updated, ignore_index = True)
+
+    return Y_all, stim_df, runs_updated
 
 
 def quant_slope(rec, timeseries):
@@ -231,3 +396,5 @@ def quant_slope(rec, timeseries):
     return slope
 
 
+
+# %%
